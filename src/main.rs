5a51962f79efa4128c74538db201cbb4f7be7e29
Jan <MapManagement@protonmail.com>
2024-11-25 21:51:22 +0100

    Implement CLI in main function

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,82 @@
-use std::fs;
+use std::{fs, path::PathBuf};
 
 use api_calls::{create_login_session, get_domain_info, update_dns_records};
+use clap::{arg, command, value_parser, Arg, ArgAction, Command};
 use configuration::Configuration;
 
 mod api_calls;
 mod api_objects;
 mod configuration;
 
-const CONFIG_FILE: &str = "example.toml";
+const CONFIG_ARG: &str = "config";
+const TEST_ARG: &str = "test";
+const VERBOSE_ARG: &str = "verbose";
+const INFO_ARG: &str = "info";
 
 #[tokio::main]
 async fn main() {
-    let config = read_config();
+    let command_matches = command!()
+        .arg(Arg::new(CONFIG_ARG)
+                .short('c')
+                .long(CONFIG_ARG)
+                .required(true)
+                .num_args(1)
+                .value_parser(value_parser!(PathBuf))
+                .help("Specify the file to read the domain configuration from")
+        )
+        .arg(
+            Arg::new(TEST_ARG)
+                .short('t')
+                .long(TEST_ARG)
+                .required(false)
+                .num_args(0)
+                .help("Verifies whether the specified configuration file is formatted correctly"),
+        )
+        .arg(
+            Arg::new(VERBOSE_ARG)
+                .short('v')
+                .long(VERBOSE_ARG)
+                .required(false)
+                .num_args(0)
+                .help("Prints more detailed information about API calls and errors."),
+        )
+        .arg(
+            Arg::new(INFO_ARG)
+                .short('i')
+                .long(INFO_ARG)
+                .required(false)
+                .num_args(0)
+                .help("Retrieves information about the specified domains and prints it in a readable format."),
+        ).get_matches();
+
+    if let Some(config_file) = command_matches.get_one::<PathBuf>(CONFIG_ARG) {
+        let config = read_config(config_file.to_path_buf());
+        let verbose_flag = command_matches.contains_id(VERBOSE_ARG);
+
+        // currently precedence over info arg
+        if command_matches.contains_id(TEST_ARG) {
+            test_branch_command(config, verbose_flag);
+            return;
+        }
+
+        if command_matches.contains_id(INFO_ARG) {
+            info_branch_command(config, verbose_flag).await;
+            return;
+        }
+
+        default_branch_command(config, verbose_flag).await;
+    }
+
+    println!("Missing config argument");
+}
+
+async fn default_branch_command(config: Configuration, verbose: bool) {
     let api_credentials = config.credentials.clone();
     let response = create_login_session(&config.credentials).await;
 
     match response {
         Ok(res) => {
             for (_, domain) in config.domains.into_iter() {
-                /*let domain_repsonse = get_domain_info(
-                    res.responsedata.apisessionid.to_owned(),
-                    domain.domain_name.to_string(),
-                    &api_credentials,
-                )
-                .await;*/
-
                 if domain.dns_records.is_none() {
                     continue;
                 }
@@ -50,8 +102,14 @@ async fn main() {
     };
 }
 
-fn read_config() -> Configuration {
-    let file_content = fs::read_to_string(CONFIG_FILE).unwrap();
+#[allow(dead_code)]
+fn test_branch_command(config: Configuration, verbose: bool) {}
+
+#[allow(dead_code)]
+async fn info_branch_command(config: Configuration, verbose: bool) {}
+
+fn read_config(config_file: PathBuf) -> Configuration {
+    let file_content = fs::read_to_string(config_file).unwrap();
 
     toml::from_str(&file_content).unwrap()
 }
